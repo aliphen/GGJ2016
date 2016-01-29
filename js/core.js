9134b42ca0d44274efd03a40c1d083d9987e26e3
Alexandre Olivier <a.olivier@criteo.com>
2016-01-29 23:57:27 +0100

    Several levels of music playing at the same time.

--- js/core.js
+++ js/core.js
@@ -7,6 +7,11 @@ var preloadTotal = 0;
 
 var stage;
 
+// Sound assets
+var soundtrackLayer1 = "soundtrackLayer1"
+var soundtrackLayer2 = "soundtrackLayer2"
+// Sound assets end
+
 function startGame()
 {
 	stage = new createjs.Stage(document.getElementById("gameCanvas"));
@@ -27,6 +32,16 @@ function preloadAssets()
 
 	//createjs.Sound.addEventListener("fileload", preloadUpdate);
 	//createjs.Sound.registerSound("media/receive.wav", "jump", 4);
+
+	createjs.Sound.addEventListener("fileload", playMusic);
+	createjs.Sound.registerSound("media/music/layer1.mp3", soundtrackLayer1);
+    createjs.Sound.registerSound("media/music/layer2.mp3", soundtrackLayer2);
+}
+function playMusic(event)
+{
+    var instance = createjs.Sound.play(event.id, {loop:-1});
+    instance.on("complete", this.handleComplete, this);
+    instance.volume = 0;
 }
 
 function preloadUpdate()
