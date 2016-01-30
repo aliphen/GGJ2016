ce4f94fbc3ad7bc7d57e75b3d4c2cb83760a293d
Alexandre Olivier <a.olivier@criteo.com>
2016-01-30 13:16:36 +0100

    Add completion to timer.

--- js/mouseZone.js
+++ js/mouseZone.js
@@ -1,15 +1,21 @@
-function MouseZone(x, y, w, h, onClick)
+function MouseZone(x, y, w, h, onClick, onComplete)
 {
     var timer;
+    var isClicked = false;
+    var clickedDuration = 0;
+    var timerInSeconds = 1;
 
     this.onClickDown = function() {
         testShape.cursor = "pointer";
-        timer = new VisualTimer(1, x + w/2, y + h/2);
+        timer = new VisualTimer(timerInSeconds, x + w/2, y + h/2);
+        isClicked = true;
         onClick();
     };
 
     this.onMouseUp = function() {
         timer.remove();
+        isClicked = false;
+        clickedDuration = 0;
     };
 
     var testShape = new createjs.Shape();
@@ -20,4 +26,12 @@ function MouseZone(x, y, w, h, onClick)
     testShape.on("pressup", this.onMouseUp);
 
     stage.addChild(testShape);
+
+
+    this.update = function(event) {
+        if (isClicked)
+            clickedDuration += event.delta;
+        if (clickedDuration > timerInSeconds)
+            onComplete();
+    }
 }
