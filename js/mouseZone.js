a6278fc5b5fb54bc38fb8e852be24bb0773db085
Raphaël Vandon <r.vandon@criteo.com>
2016-01-30 13:55:04 +0100

    chain timers on objects

--- js/mouseZone.js
+++ js/mouseZone.js
@@ -4,7 +4,9 @@ function MouseZone(x, y, w, h, onClick)
 
     var timer;
     var clickedDuration = 0;
-    var timerInSeconds = 1;
+    var activeDuration = 0;
+    var clickTimeInSec = 1;
+    var activeTimeInSec = 3;
 
     var testShape = new createjs.Shape();
     testShape.graphics.beginFill("rgba(255,0,0,0.5)").drawRect(x, y, w, h);
@@ -15,7 +17,7 @@ function MouseZone(x, y, w, h, onClick)
 
     testShape.on("mousedown", function() {
         testShape.cursor = "pointer";
-        timer = new VisualTimer(timerInSeconds, x + w/2, y + h/2, incrementalTimer);
+        timer = new VisualTimer(clickTimeInSec, x + w/2, y + h/2, incrementalTimer);
         state = "clicked";
         onClick();
     });
@@ -31,10 +33,19 @@ function MouseZone(x, y, w, h, onClick)
     this.update = function(event) {
         if (state == "clicked") {
             clickedDuration += event.delta/1000;
-            if (clickedDuration > timerInSeconds) {
+            if (clickedDuration > clickTimeInSec) {
                 timer.remove();
                 timer = new VisualTimer(3, x + w / 2, y + h / 2, decrementalTimer);
                 state = "active"
+            }
+        }
+        if(state == "active")
+        {
+            activeDuration += event.delta/1000;
+            if (activeDuration > activeTimeInSec) {
+                timer.remove();
+                state = "inactive";
+                activeDuration = 0;
             }
         }
     }
