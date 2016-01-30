d63fb6e5eec5057ddd3c8da1cadbaa70c191b17e
Raphaël Vandon <r.vandon@criteo.com>
2016-01-31 00:04:53 +0100

    add assets for 3 new objects

--- js/core.js
+++ js/core.js
@@ -16,6 +16,9 @@ var imgBg;
 var imgFlower;
 var imgDebug;
 var imgYeux;
+var imgWindow;
+var imgPhone;
+var imgAqua;
 // Images assets end
 
 // Sound assets
@@ -46,6 +49,9 @@ function preloadAssets()
     imgFlower = loadImg("Fleur-01.png");
     imgDebug = loadImg("debug.png");
     imgYeux = loadImg("lesyeux.png");
+    imgWindow = loadImg("window.png");
+    imgPhone = loadImg("phone.png");
+    imgAqua = loadImg("aqua.png");
 
     createjs.Sound.addEventListener("fileload", playMusicLayers);
     createjs.Sound.registerSound("media/music/layer1.mp3", soundtrackLayer1);
@@ -91,18 +97,48 @@ function launchGame()
         images: [imgFlower],
         frames: {height: 56, width: 38},
         animations: {
-            still: [0, 0],
+            still: 0,
             click: [1, 4, "active", 0.1],
-            active: [4, 4],
+            active: 4,
             decay: [5, 7, "still", 0.1]
         }
     });
+    var spSheetWindow = new createjs.SpriteSheet({
+        images: [imgWindow],
+        frames: {height: 157, width: 250},
+        animations: {
+            still: 0,
+            click: [1, 4, "active", 0.08],
+            active: 4,
+            decay: [5, 7, "still", 0.08]
+        }
+    });
+    var spSheetPhone = new createjs.SpriteSheet({
+        images: [imgPhone],
+        frames: {height: 49, width: 27},
+        animations: {
+            still: 0,
+            click: [1, 5, "active", 0.1],
+            active: 6,
+            decay: [7, 11, "still", 0.1]
+        }
+    });
+    var spSheetAqua = new createjs.SpriteSheet({
+        images: [imgAqua],
+        frames: {height: 42, width: 108},
+        animations: {
+            still: 0,
+            click: [1, 37, "active", 0.78],
+            active: 38,
+            decay: [39, 74, "still", 0.78]
+        }
+    });
 
-    createClickable(510, 149, spSheetFlower, spSheetYeux, "flower"); //pot, dites non à la drogue
-    createClickable(50, 20, spSheetFlower, spSheetYeux, "photo"); //frame 1080p 60fps
-    createClickable(200, 50, spSheetFlower, spSheetYeux, "window"); //maitre window
-    createClickable(425, 149, spSheetFlower, spSheetYeux, "aquarium"); //aqua. Hi Ken !
-    createClickable(1000, 120, spSheetFlower, spSheetYeux, "phone"); //phone, ouh ouh ouh, banana phone
+    createClickable(550,  149, spSheetFlower, spSheetYeux, "flower"  );
+    createClickable(50,   20,  spSheetFlower, spSheetYeux, "photo"   );
+    createClickable(170,  35,  spSheetWindow, spSheetYeux, "window"  );
+    createClickable(430,  163, spSheetAqua,   spSheetYeux, "aquarium");
+    createClickable(1000, 120, spSheetPhone,  spSheetYeux, "phone"   );
 
     player = new Player(imgPlayer, [900, 60, 1250]);
     player.start = true;
