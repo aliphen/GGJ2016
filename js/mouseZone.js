bc138c060a58e609f9448883ca3c1c687d8b4d08
Raphaël Vandon <r.vandon@criteo.com>
2016-01-30 13:42:05 +0100

    inline click methods

--- js/mouseZone.js
+++ js/mouseZone.js
@@ -6,31 +6,27 @@ function MouseZone(x, y, w, h, onClick)
     var clickedDuration = 0;
     var timerInSeconds = 1;
 
-    this.onClickDown = function() {
+    var testShape = new createjs.Shape();
+    testShape.graphics.beginFill("rgba(255,0,0,0.5)").drawRect(x, y, w, h);
+    testShape.cursor = "pointer";
+
+    stage.addChild(testShape);
+    eltsToUpdate.push(this);
+
+    testShape.on("mousedown", function() {
         testShape.cursor = "pointer";
         timer = new VisualTimer(timerInSeconds, x + w/2, y + h/2, incrementalTimer);
         state = "clicked";
         onClick();
-    };
+    });
 
-    this.onMouseUp = function() {
+    testShape.on("pressup", function() {
         if(state == "clicked") {
             timer.remove();
             state = "inactive";
         }
         clickedDuration = 0;
-    };
-
-    var testShape = new createjs.Shape();
-    testShape.graphics.beginFill("rgba(255,0,0,0.5)").drawRect(x, y, w, h);
-    testShape.cursor = "pointer";
-
-    testShape.on("mousedown", this.onClickDown);
-    testShape.on("pressup", this.onMouseUp);
-
-    stage.addChild(testShape);
-    eltsToUpdate.push(this);
-
+    });
 
     this.update = function(event) {
         if (state == "clicked") {
