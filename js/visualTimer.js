2e1d4a74bf2d8f0040ee3a23712446a4ac223a45
Raphaël Vandon <r.vandon@criteo.com>
2016-01-30 14:49:06 +0100

    decremental timer decreases counterclockwise

--- js/visualTimer.js
+++ js/visualTimer.js
@@ -17,12 +17,14 @@ function VisualTimer(duration, x, y, type)
 
         var prevarc = arc;
         var arcIncrement = deltaT*Math.PI*2 / (duration*1000);
-        arc = arc + arcIncrement;
-        if(type == incrementalTimer)
+        if(type == incrementalTimer) {
+            arc = arc + arcIncrement;
             shape.graphics.setStrokeStyle(8).beginStroke("blue").arc(x, y, radius, prevarc, arc);
+        }
         else{
+            arc = arc - arcIncrement;
             shape.graphics.clear();
-            shape.graphics.setStrokeStyle(8).beginStroke("blue").arc(x, y, radius, arc, 3*Math.PI/2);
+            shape.graphics.setStrokeStyle(8).beginStroke("blue").arc(x, y, radius, -5*Math.PI/2, arc);
         }
     }
 
