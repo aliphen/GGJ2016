eb92fec8ec3232101079842e9d5786040977b4f4
Alexandre Olivier <a.olivier@criteo.com>
2016-01-31 04:27:16 +0100

    Improved endgame with fade to black.

--- js/endGame.js
+++ js/endGame.js
@@ -5,8 +5,35 @@ function EndGame() {
     this.finalBackGround.alpha = 0;
     stage.addChild(this.finalBackGround);
 
+    var countdownToBlack = 150;
+    var rectangleToCoverScene;
+
     this.update = function(event) {
-        if (this.finalBackGround.alpha < 1)
+        if (this.finalBackGround.alpha < 1) {
             this.finalBackGround.alpha += 0.005;
+            return;
+        }
+
+        // once we have the full colored screen, wait 10 sec
+        if (countdownToBlack > 0)
+            countdownToBlack--;
+        else if (countdownToBlack == 0)
+        {
+            // stop the music and fade to black
+            countdownToBlack--;
+            musicHandler.fadeMusic(soundtrackLayer1, MusicStates.FadingOut);
+            musicHandler.fadeMusic(soundtrackLayer3, MusicStates.FadingOut);
+            rectangleToCoverScene = new createjs.Shape();
+            rectangleToCoverScene.graphics.beginFill("#000000").drawRect(0, 0, 1200, 700);
+            rectangleToCoverScene.visible = true;
+            rectangleToCoverScene.alpha = 0;
+            stage.addChild(rectangleToCoverScene);
+        }
+        else
+        {
+            if (rectangleToCoverScene.alpha < 1)
+                rectangleToCoverScene.alpha += 0.01;
+        }
+
     }
 }
